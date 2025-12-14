0ca4a4a9b0c0941ecca95ac15053d54fad954c69
JOHN <john@Mac.lan>
2025-12-14 18:13:40 -0500

    Fix missing imports in MainActivityLaunchTest

--- app/src/androidTest/java/com/bitcoinerrorlog/skywriter/MainActivityLaunchTest.kt
+++ app/src/androidTest/java/com/bitcoinerrorlog/skywriter/MainActivityLaunchTest.kt
@@ -7,6 +7,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert.*
 import android.util.Log
 import android.view.View
 import java.util.concurrent.CountDownLatch
